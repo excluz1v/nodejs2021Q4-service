c8b0f13898039bf8fd0561b23266dce8c75c64d9
excluz1v <bmd4work@gmail.com>
2021-12-05 00:05:18 +0600

    feat: create user routes functional

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,53 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+const taskRepo = require('../tasks/tasks.memory.repository');
+
+let users = [];
+
+const getAll = async () => users;
+
+const postUser = async (user) => {
+  const newUser = new User(user);
+  users = [...users, newUser];
+  return newUser;
 };
 
-module.exports = { getAll };
+const getUserById = async (id) => {
+  const result = await users.find((user) => user.id === id);
+  return result;
+};
+const getUserByLogin = async (login) => {
+  const result = await users.find((user) => user.login === login);
+  return result;
+};
+
+const updateUserById = async (id, userCredentials) => {
+  const result = await users.find((user) => user.id === id);
+  if (result === undefined) return false;
+
+  let userIndex;
+  users = users.map((user, index) => {
+    if (user.id === id) {
+      userIndex = index;
+      return { ...user, ...userCredentials };
+    }
+    return user;
+  });
+  return users[userIndex];
+};
+
+const deleteUserById = async (id) => {
+  const isExist = await getUserById(id);
+  if (isExist === undefined) return false;
+  users = await users.filter((user) => user.id !== id);
+  taskRepo.deleteAssignedUsers(id);
+  return true;
+};
+
+module.exports = {
+  getAll,
+  postUser,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+  getUserByLogin,
+};
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,28 @@
 const usersRepo = require('./user.memory.repository');
+const User = require('./user.model');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () => usersRepo.getAll();
 
-module.exports = { getAll };
+const postUser = async (userCredentials) => {
+  const newUser = await usersRepo.postUser(userCredentials);
+  return User.toResponse(newUser);
+};
+
+const getUserById = async (id) => {
+  const result = await usersRepo.getUserById(id);
+  if (result === undefined) return false;
+  return User.toResponse(result);
+};
+
+const putUser = async (id, userCredentials) => {
+  const updatedUser = await usersRepo.updateUserById(id, userCredentials);
+  if (updatedUser === false) return false;
+  return User.toResponse(updatedUser);
+};
+
+const deleteUserById = async (id) => {
+  const result = await usersRepo.deleteUserById(id);
+  return result;
+};
+
+module.exports = { getAll, postUser, getUserById, putUser, deleteUserById };
