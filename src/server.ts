0e892bd4c7b6e7f33c0678363012163898700b89
excluz1v <bmd4work@gmail.com>
2022-01-05 15:46:16 +0600

    feat: add dockerfiles, docker-compose file

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,27 @@
-import fastify, { FastifyInstance} from 'fastify';
+import fastify, { FastifyInstance } from 'fastify';
 import swagger from 'fastify-swagger';
 import path from 'path';
 import fs from 'fs';
-import {userRoutes} from './resources/users/user.router';
+import { userRoutes } from './resources/users/user.router';
 // const fastifyPlugin from './resources/login/auth');
 // const { AuthRouter } from './resources/login/auth.route');
-import {config}  from './common/config';
-import {boardRoutes} from './resources/boards/boards.router';
-import {taskRoutes} from './resources/tasks/task.router';
+import { config } from './common/config';
+import { boardRoutes } from './resources/boards/boards.router';
+import { taskRoutes } from './resources/tasks/task.router';
 import { logger } from './logger';
 
-
-const server:FastifyInstance=fastify({logger})
+const server: FastifyInstance = fastify({ logger });
 
 // if (AUTH_MODE) {
 //   server.register(fastifyPlugin);
 //   server.register(AuthRouter);
 // }
 
-const errorPath=path.resolve(__dirname,'../errors.log')
+const errorPath = path.resolve(__dirname, '../errors.log');
 
 const start = async () => {
   try {
-  await server.register(swagger, {
+    await server.register(swagger, {
       mode: 'static',
       routePrefix: '/doc',
       specification: {
@@ -34,37 +33,39 @@ const start = async () => {
       },
       exposeRoute: true,
     });
-   await server.register(userRoutes);
-   await server.register(boardRoutes);
-   await server.register(taskRoutes);
+    await server.register(userRoutes);
+    await server.register(boardRoutes);
+    await server.register(taskRoutes);
     await server.listen(config.PORT);
-    process.on('uncaughtException',(e)=>{
+    process.on('uncaughtException', (e) => {
       const getDateTime = (): string => new Date().toLocaleString();
       const errorStream = fs.createWriteStream(errorPath, 'utf-8');
-      const errorMessage = `uncaughtException ERROR: ${getDateTime()} ${e.message}\n`;
-    errorStream.write(errorMessage, () => {
-      console.log(errorMessage); 
+      const errorMessage = `uncaughtException ERROR: ${getDateTime()} ${
+        e.message
+      }\n`;
+      errorStream.write(errorMessage, () => {
+        console.log(errorMessage);
+      });
     });
-    })
-    process.on('unhandledRejection',(e:Error)=>{
+
+    process.on('unhandledRejection', (e: Error) => {
       const getDateTime = (): string => new Date().toLocaleString();
       const errorStream = fs.createWriteStream(errorPath, 'utf-8');
-      const errorMessage = `unhandledRejection ERROR: ${getDateTime()} ${e.message}\n`;
-    errorStream.write(errorMessage, () => {
-      console.log(errorMessage); 
+      const errorMessage = `unhandledRejection ERROR: ${getDateTime()} ${
+        e.message
+      }\n`;
+      errorStream.write(errorMessage, () => {
+        console.log(errorMessage);
+      });
     });
-    })
 
     // TEST ERROR
     // await Promise.reject(Error('Oops!'))
 
     // TEST ERROR
-// throw new Error('Oops!');
-
+    // throw new Error('Oops!');
   } catch (err) {
     server.log.error(err);
   }
 };
-
-
-void start()
+void start();
