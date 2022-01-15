2f0612ce6816df8a9e0a130148f31a44ab66cfa1
excluz1v <bmd4work@gmail.com>
2022-01-15 16:33:13 +0600

    init: start project

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify, { FastifyInstance } from 'fastify';
 import swagger from 'fastify-swagger';
 import path from 'path';
 import fs from 'fs';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
 import { userRoutes } from './resources/users/user.router';
 // const fastifyPlugin from './resources/login/auth');
 // const { AuthRouter } from './resources/login/auth.route');
@@ -10,8 +12,27 @@ import { boardRoutes } from './resources/boards/boards.router';
 import { taskRoutes } from './resources/tasks/task.router';
 import { logger } from './logger';
 
+import { User } from './entity/User';
+
 const server: FastifyInstance = fastify({ logger });
 
+createConnection()
+  .then(async (connection) => {
+    console.log('Inserting a new user into the database...');
+    const user = new User();
+    user.firstName = 'Timber';
+    user.lastName = 'Saw';
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log(`Saved a new user with id:%${user.id} `);
+
+    console.log('Loading users from the database...');
+    const users = await connection.manager.find(User);
+    console.log('Loaded users: ', users);
+
+    console.log('Here you can setup and run express/koa/any other framework.');
+  })
+  .catch((error) => console.log(error));
 // if (AUTH_MODE) {
 //   server.register(fastifyPlugin);
 //   server.register(AuthRouter);
