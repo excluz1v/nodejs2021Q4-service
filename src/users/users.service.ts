b2ddfddd2024301b8ef19162bbe6420c734ba170
Dmitriy <bmd4work@gmail.com>
2022-02-01 17:21:55 +0600

    feat: implement getAll and create user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Repository } from 'typeorm';
 import { CreateuserDto } from './create-user.dto';
+import { User, UserEntity } from './user.entity';
+import bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(@InjectModel(User) private userRepository: Repository<User>) { }
+
+  hashPassword(pass: string) {
+    return bcrypt.hasnSync(pass, 10);
+  }
   async createUser(dto: CreateuserDto) {
-    const user = await this.userRepository.create(dto);
-    return user;
+    const { name, login } = dto;
+    const password = this.hashPassword(dto.password);
+    const user = await this.userRepository.save({ name, login, password });
+    return new UserEntity(user);
   }
   async getAllusers() {
     const users = await this.userRepository.find();
-    return users;
+    return users.map((user) => new UserEntity(user));
   }
 }
