438eaf52baac402a9c46bd3a8f1f3e9caa9634dd
Dmitriy <bmd4work@gmail.com>
2022-02-01 18:48:19 +0600

    feat: create userTable migration

---
+++ src/db/migration/userTable.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+export class UserTable implements MigrationInterface {
+  public async up(queryRunner: QueryRunner) {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'text',
+          },
+          {
+            name: 'login',
+            type: 'text',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'text',
+          },
+        ],
+      }),
+      true,
+    );
+
+    const adminPsswordHash = bcrypt.hashSync('admin', 10);
+
+    await queryRunner.query(
+      `INSERT INTO "user" (name, login, password) VALUES ($1, $1, $2);`,
+      ['admin', adminPsswordHash],
+    );
+  }
+
+  public async down(queryRunner: QueryRunner) {
+    await queryRunner.dropTable('user');
+  }
+}
