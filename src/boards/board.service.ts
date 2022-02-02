de0d3e8358825ff32df521de91b48bb184986f26
Dmitriy <bmd4work@gmail.com>
2022-02-02 21:34:27 +0600

    feat: realize board service logic

--- src/boards/board.service.ts
+++ src/boards/board.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoardColumn } from 'src/columns/column.entity';
+import { Repository } from 'typeorm';
+import { Board } from './board.entity';
+import { CreateBoardDto, UpdateBoardDto } from './create-board.dto';
 
 @Injectable()
-export class BoardsService { }
+export class BoardsService {
+  constructor(
+    @InjectRepository(Board) private boardsRepository: Repository<Board>,
+    @InjectRepository(BoardColumn)
+    private readonly columnRepository: Repository<BoardColumn>,
+  ) { }
+
+  async create(createBoardDto: CreateBoardDto) {
+    const newBoard = this.boardsRepository.create(createBoardDto);
+    const columns = newBoard.columns;
+    columns.map((column) => this.columnRepository.create(column));
+    newBoard.columns = [...columns];
+    return this.boardsRepository.save(newBoard);
+  }
+  async findAll() {
+    return this.boardsRepository.find({
+      relations: ['columns'],
+    });
+  }
+
+  async findOne(id: string) {
+    const board = await this.boardsRepository.findOne(id, {
+      relations: ['columns'],
+    });
+    if (!board) throw new NotFoundException();
+    return board;
+  }
+
+  async update(id: string, updateBoardDto: UpdateBoardDto) {
+    const board = await this.boardsRepository.findOne(id);
+    if (!board) {
+      throw new NotFoundException();
+    }
+    this.boardsRepository.merge(board, updateBoardDto);
+    await this.boardsRepository.save(board);
+    return board;
+  }
+  async remove(id: string) {
+    return this.boardsRepository.delete(id);
+  }
+}
