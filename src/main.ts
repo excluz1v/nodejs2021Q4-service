cfaec714d5aa376f2ec7a0f33a2d4e50b5eb8c33
Dmitriy <bmd4work@gmail.com>
2022-02-05 23:34:11 +0600

    feat: implements logging

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn'],
+  });
   app.useGlobalGuards(new JwtAuthGuard());
   await app.listen(4000);
 }
