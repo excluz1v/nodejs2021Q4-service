a71eb78cb5a4e0caad6a1e5c08a7b658a5028bfc
Dmitriy <bmd4work@gmail.com>
2022-02-05 20:29:36 +0600

    feat: implement file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -33,6 +35,9 @@ import { FilesModule } from './files/files.module';
       synchronize: false,
       migrations: [PostRefactoring1642325483900],
       migrationsRun: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, 'static'),
     }),
     UsersModule,
     BoardModule,
